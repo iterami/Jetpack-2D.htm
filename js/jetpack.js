8177b68076dcdecd6e838995fb1be7e6736eea5a
Jan Horava <czechinthemail@gmail.com>
2015-04-12 10:25:30 -0700

    improved formatting

--- js/jetpack.js
+++ js/jetpack.js
@@ -426,25 +426,26 @@ function setmode(newmode){
           settings['ms-per-frame']
         );
 
-    // Main menu mode.
-    }else{
-        buffer = 0;
-        canvas = 0;
-
-        document.getElementById('page').innerHTML='<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><a onclick=setmode(1)>Cave Corridor</a></div><hr><div class=c>Best: '
-          + best
-          + '<br><a onclick=reset_best()>Reset Best</a></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c>Jetpack:<ul><li><input disabled style=border:0 value=Click>Activate<li><input id=jetpack-key maxlength=1 value='
-          + settings['jetpack-key'] + '>Activate</ul><input disabled style=border:0 value=ESC>Main Menu<br><input id=restart-key maxlength=1 value='
-          + settings['restart-key'] + '>Restart</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
-          + settings['audio-volume'] + '>Audio<br><input id=color type=color value='
-          + settings['color'] + '>Color<br><input id=gravity value='
-          + settings['gravity'] + '>Gravity<br>Jetpack:<ul><li><input id=jetpack-power value='
-          + settings['jetpack-power'] + '>Power<li><input id=speed value='
-          + settings['speed'] + '>Speed</ul><input id=ms-per-frame value='
-          + settings['ms-per-frame'] + '>ms/Frame<br>Obstacle:<ul><li><input id=obstacle-frequency value='
-          + settings['obstacle-frequency'] + '>Frequency<li><input id=obstacle-increase value='
-          + settings['obstacle-increase'] + '>Increase</ul><a onclick=reset()>Reset Settings</a></div></div>';
+        return;
     }
+
+    // Main menu mode.
+    buffer = 0;
+    canvas = 0;
+
+    document.getElementById('page').innerHTML='<div style=display:inline-block;text-align:left;vertical-align:top><div class=c><a onclick=setmode(1)>Cave Corridor</a></div><hr><div class=c>Best: '
+      + best
+      + '<br><a onclick=reset_best()>Reset Best</a></div></div><div style="border-left:8px solid #222;display:inline-block;text-align:left"><div class=c>Jetpack:<ul><li><input disabled style=border:0 value=Click>Activate<li><input id=jetpack-key maxlength=1 value='
+      + settings['jetpack-key'] + '>Activate</ul><input disabled style=border:0 value=ESC>Main Menu<br><input id=restart-key maxlength=1 value='
+      + settings['restart-key'] + '>Restart</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
+      + settings['audio-volume'] + '>Audio<br><input id=color type=color value='
+      + settings['color'] + '>Color<br><input id=gravity value='
+      + settings['gravity'] + '>Gravity<br>Jetpack:<ul><li><input id=jetpack-power value='
+      + settings['jetpack-power'] + '>Power<li><input id=speed value='
+      + settings['speed'] + '>Speed</ul><input id=ms-per-frame value='
+      + settings['ms-per-frame'] + '>ms/Frame<br>Obstacle:<ul><li><input id=obstacle-frequency value='
+      + settings['obstacle-frequency'] + '>Frequency<li><input id=obstacle-increase value='
+      + settings['obstacle-increase'] + '>Increase</ul><a onclick=reset()>Reset Settings</a></div></div>';
 }
 
 function update_best(){
