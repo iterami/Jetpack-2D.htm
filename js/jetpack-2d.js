2218da2fc29e91be3c4a8529e1926debac31f720
Jan Horava <czechinthemail@gmail.com>
2015-06-19 22:26:54 -0700

    added corrdior-height setting

--- js/jetpack-2d.js
+++ js/jetpack-2d.js
@@ -10,9 +10,9 @@ function draw(){
     buffer.fillStyle = '#000';
     buffer.fillRect(
       0,
-      y - 250,
+      y - settings['corridor-height'] / 2,
       width,
-      500
+      settings['corridor-height']
     );
 
     // Draw player body.
@@ -133,8 +133,8 @@ function draw(){
 
 function logic(){
     // Check if player is outside of game boundaries.
-    if(player_y + 25 > 250
-      || player_y - 25 < -250){
+    if(player_y + 25 > settings['corridor-height'] / 2
+      || player_y - 25 < -settings['corridor-height'] / 2){
         game_running = false;
         update_best();
     }
@@ -153,7 +153,8 @@ function logic(){
           0,
           [
             x + obstalce_width,
-            Math.floor(Math.random() * 500) - 250,
+            Math.floor(Math.random() * settings['corridor-height'])
+              - settings['corridor-height'] / 2,
             obstalce_width,
             Math.floor(Math.random() * 15) + 20,
           ]
@@ -241,6 +242,7 @@ function reset(){
 
     document.getElementById('audio-volume').value = 1;
     document.getElementById('color').value = '#009900';
+    document.getElementById('corridor-height').value = 500;
     document.getElementById('gravity').value = 1;
     document.getElementById('jetpack-key').value = 'W';
     document.getElementById('jetpack-power').value = 2;
@@ -314,6 +316,7 @@ function save(){
     }
 
     ids = {
+      'corridor-height': 500,
       'gravity': 1,
       'jetpack-power': 2,
       'ms-per-frame': 30,
@@ -382,7 +385,8 @@ function setmode(newmode){
       + settings['jetpack-key'] + '>Activate</ul><input disabled style=border:0 value=ESC>Main Menu<br><input id=restart-key maxlength=1 value='
       + settings['restart-key'] + '>Restart</div><hr><div class=c><input id=audio-volume max=1 min=0 step=.01 type=range value='
       + settings['audio-volume'] + '>Audio<br><input id=color type=color value='
-      + settings['color'] + '>Color<br><input id=gravity value='
+      + settings['color'] + '>Color<br><input id=corridor-height value='
+      + settings['corridor-height'] + '>Corridor Height<br><input id=gravity value='
       + settings['gravity'] + '>Gravity<br>Jetpack:<ul><li><input id=jetpack-power value='
       + settings['jetpack-power'] + '>Power<li><input id=speed value='
       + settings['speed'] + '>Speed</ul><input id=ms-per-frame value='
@@ -425,6 +429,7 @@ var player_y = 0;
 var settings = {
   'audio-volume': parseFloat(window.localStorage.getItem('Jetpack-2D.htm-audio-volume')) || 1,
   'color': window.localStorage.getItem('Jetpack-2D.htm-color') || '#009900',
+  'corridor-height': window.localStorage.getItem('Jetpack-2D.htm-corridor-height') || 500,
   'gravity': parseFloat(window.localStorage.getItem('Jetpack-2D.htm-gravity')) || 1,
   'jetpack-key': window.localStorage.getItem('Jetpack-2D.htm-jetpack-key') || 'W',
   'jetpack-power': parseFloat(window.localStorage.getItem('Jetpack-2D.htm-jetpack-power')) || 2,
