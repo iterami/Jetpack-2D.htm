52ad8e00ef7d0275deb3d795968de0d965f656ed
Jan Hořava <czechinthemail@gmail.com>
2022-04-09 01:23:02 -0700

    added player check

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 'use strict';
 
 function repo_drawlogic(){
+    if(!entity_entities['player']){
+        return;
+    }
+
     canvas_buffer.save();
     canvas_buffer.translate(
       canvas_properties['width-half'],
