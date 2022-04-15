2dd40d878d1ead29dd3f1e46566e7de3fab56b3a
Jan Hořava <czechinthemail@gmail.com>
2022-04-14 20:23:41 -0700

    reset obstacle_counter on load

--- js/data.js
+++ js/data.js
@@ -1,10 +1,11 @@
 'use strict';
 
 function load_data(id){
+    core_mode = 1;
     frame_counter = 0;
     frames_per_obstacle = core_storage_data['obstacle-frequency'];
-    core_mode = 1;
     half_corridor_height = core_storage_data['corridor-height'] / 2;
+    obstacle_counter = 0;
 
     entity_create({
       'id': 'player',
