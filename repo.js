ca71bf47413131dc4466409068a82c388a12bddd
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 13:37:19 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,20 @@
 'use strict';
 
+function load_data(id){
+    core_mode = 1;
+    frame_counter = 0;
+    frames_per_obstacle = core_storage_data['obstacle-frequency'];
+    half_corridor_height = core_storage_data['corridor-height'] / 2;
+    obstacle_counter = 0;
+
+    entity_create({
+      'id': 'player',
+      'types': [
+        'player',
+      ],
+    });
+}
+
 function repo_drawlogic(){
     if(!entity_entities['player']){
         return;
